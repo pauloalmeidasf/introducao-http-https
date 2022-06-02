28901545c75a99a9dadacdd4b58ed9825cffdedb
Paulo <paulo.almeidasantosferreira@gmail.com>
2022-06-02 20:28:15 -0300

    Initial commit

---
+++ HttpExample/src/com/httpexamples/postRequest.java
@@ -0,0 +1,33 @@
+package com.httpexamples;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Path;
+import java.time.Duration;
+
+public class postRequest {
+
+    public static final String URL_POST = "http://httpbin.org/forms/post";
+
+    public static final String FILE_JSON = "pedido.json";
+
+    public static void main(String[] args) throws IOException, InterruptedException{
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofFile(Path.of(FILE_JSON)))
+                .timeout(Duration.ofSeconds(10))
+                .uri(URI.create(URL_POST))
+                .build();
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
+
+    }
+}
